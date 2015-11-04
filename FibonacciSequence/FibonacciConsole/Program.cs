92551b8a62f1ba43b09fe033b2eec21a58d6a975
nick.semenyk <nick.semenyk@gmail.com>
2015-11-05 01:22:45 +0300

    1

---
+++ FibonacciSequence/FibonacciConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FibonacciSequence;
+
+namespace FibonacciConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciNumber f = new FibonacciNumber(25);
+            foreach (long number in f)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FibonacciSequence/FibonacciSequence/FibonacciNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    public class FibonacciNumber : IEnumerable<long>
+    {
+        private int count;
+
+        public FibonacciNumber(int count)
+        {
+            this.count = count;
+        }
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            long a = 1;
+            long b = 0;
+            long c = 0;
+            for (int i = 0; i<count; i++)
+            {
+                yield return c;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
