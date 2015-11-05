513a3cf41d4a54f80050a700c106e51103fbcd09
nick.semenyk <nick.semenyk@gmail.com>
2015-11-05 05:34:25 +0300

    impossible to use SOS.dll right now

---
+++ WinDbg/TestDbg/TestDbg/Point2D.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDbg
+{
+    public struct Point2D : IComparable
+    {
+        public int X;
+        public int Y;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            Point2D otherPoint2D = (Point2D)obj;
+            if (X > otherPoint2D.X)
+                return 1;
+            if (X < otherPoint2D.X)
+                return -1;
+            if (Y > otherPoint2D.Y)
+                return 1;
+            if (Y < otherPoint2D.Y)
+                return -1;
+            return 0;
+        }
+    }
+
+    public class Point2DClass : IComparable
+    {
+        public int X;
+        public int Y;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            Point2DClass otherPoint2D = (Point2DClass)obj;
+            if (X > otherPoint2D.X)
+                return 1;
+            if (X < otherPoint2D.X)
+                return -1;
+            if (Y > otherPoint2D.Y)
+                return 1;
+            if (Y < otherPoint2D.Y)
+                return -1;
+            return 0;
+        }
+    }
+
+}
---
+++ WinDbg/TestDbg/TestDbgConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDbg;
+
+namespace TestDbgConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2D p = new Point2D();
+            p.X = 100;
+            p.Y = 100;
+            Point2D o = new Point2D();
+            o.X = 100;
+            o.Y = 100;
+            int result = p.CompareTo(o);
+            Console.WriteLine(result);
+            //
+            Point2DClass q = new Point2DClass();
+            q.X = 100;
+            q.Y = 100;
+            Point2DClass w = new Point2DClass();
+            w.X = 100;
+            w.Y = 100;
+            result = q.CompareTo(w);
+            Console.WriteLine(result);
+        }
+    }
+}
